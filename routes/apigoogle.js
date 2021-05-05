39731779d2a32ce9959c9817d55eb81afee147cb
AnhPhan49 <phanducanhkg@gmail.com>
2021-05-05 17:30:01 +0700

    update account student

--- routes/apigoogle.js
+++ routes/apigoogle.js
@@ -3,6 +3,7 @@ const Router = express.Router()
 const {OAuth2Client} =  require('google-auth-library')
 const jwt = require('jsonwebtoken')
 const AccountModel = require('../models/AccountModel')
+const mongoose = require('mongoose')
 const client = new OAuth2Client("173768816222-a3th16lqbckuej5epilhsnv3tg0l031q.apps.googleusercontent.com")
 
 Router.get('/student',(req,res)=>{
@@ -19,16 +20,16 @@ Router.get('/student',(req,res)=>{
 
 Router.post('/googlelogin',(req,res)=>{
     const{tokenId}=req.body
-
+    let original_id = mongoose.Types.ObjectId()
     client.verifyIdToken({idToken :tokenId,audience:"173768816222-a3th16lqbckuej5epilhsnv3tg0l031q.apps.googleusercontent.com"})
     .then(response=>{
         const {email_verified,name,email,picture}= response.payload
-        if(email_verified){
-            
+
+        if(email_verified){        
             AccountModel.findOne({user:email}).exec((err,user)=>{
                 if(err){
-                    return res.status(400).json({
-                        code:2,message:err
+                    return res.json({
+                        code:2,message:err.message
                     })
                 } else{
                     
@@ -43,8 +44,10 @@ Router.post('/googlelogin',(req,res)=>{
                             faculty:user.faculty
                         },JWT_SECRET,{expiresIn:"3h"})
                         res.json({code:0,message:"Đăng nhập thành công",token:token})
+                        
                     }else{
                         let newAccount = new AccountModel({
+                            _id:original_id,
                             user:email,
                             user_name:name,
                             avatar:picture,
@@ -52,20 +55,20 @@ Router.post('/googlelogin',(req,res)=>{
                         })
                         newAccount.save((err,data)=>{
                             if(err){
-                                return res.status(400).json({
-                                    code:2,message:err
+                                return res.json({
+                                    code:2,message:err.message
                                 })
                             }
                             const {JWT_SECRET} = process.env
                             const token = jwt.sign({
-                                id:user.id,
-                                user:user.user,
-                                user_name:user.user_name,
-                                avatar:user.avatar, 
-                                role:user.role,
-                                faculty:user.faculty
-                            },JWT_SECRET,{expiresIn:"3d"})
-                            res.json({code:0,message:"Đăng nhập thành công",token:token})
+                                id:data.id,
+                                user:data.user,
+                                user_name:data.user_name,
+                                avatar:data.avatar, 
+                                role:data.role,
+                                faculty:data.faculty
+                                },JWT_SECRET,{expiresIn:"3d"})
+                                res.json({code:0,message:"Đăng nhập thành công",token:token})
                         })
                     }
                 }
