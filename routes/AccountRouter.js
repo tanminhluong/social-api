033ddedff794f4fd9ea77a8484ee74af42369408
AnhPhan49 <phanducanhkg@gmail.com>
2021-05-07 02:21:46 +0700

    new datetime

--- routes/AccountRouter.js
+++ routes/AccountRouter.js
@@ -140,7 +140,7 @@ Router.put('/repassword',CheckLogin,async(req,res)=>{
         if (role === "student"){
             throw new Error("Tài khoản không cung cấp mật khẩu")
         }
-        let password =bcrypt.hash(repassword,10)
+        let password = await bcrypt.hash(repassword,10)
         let data = {
             password: password,
         }
