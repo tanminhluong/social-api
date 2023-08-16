15d00d833730fc4a27e5871811b3aa756039204b
Tấn Minh Lương <tanminhluong99@gmail.com>
2023-08-16 10:10:23 +0700

    ...

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const app = express();
 connectDB();
 
 app.all("/", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:9000");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
